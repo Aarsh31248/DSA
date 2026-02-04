42f1d7b20fc338751ca9fbda5b724f3de72b6c4f
Aarsh Singh <starfire9599@gmail.com>
2026-02-04 17:02:17 +0530

    solved power of 2 question using recursion

---
+++ recursion/powerOf2.js
@@ -0,0 +1,11 @@
+function power(n) {
+  if (n == 1) {
+    return true;
+  } else if (n < 1 || n % 2 !== 0) {
+    return false;
+  }
+
+  return power(n / 2);
+}
+const result = power(16);
+console.log(result);
