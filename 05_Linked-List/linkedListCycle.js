33493bb22b806759e2c25a2fe1c106203bcd4371
Aarsh Singh <starfire9599@gmail.com>
2026-02-12 12:31:15 +0530

    Approach 2 using Floyd's Cycle Finding Algorithm

--- 05_Linked-List/linkedListCycle.js
+++ 05_Linked-List/linkedListCycle.js
@@ -13,3 +13,18 @@ function linkedListCycle(head) {
   return false;
 }
 // Time complexity: O(n), Space complexity: O(n)
+
+// Approach 2 using Floyd's Cycle Finding Algorithm
+function linkedListCycle(head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    
+    if (slow === fast) return true;
+  }
+  return false;
+}
+// Time complexity: O(n), Space complexity: O(1)
