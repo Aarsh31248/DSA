f3b1e8d46c6d2509a0facbea864c7e2bd304d863
Aarsh Singh <starfire9599@gmail.com>
2026-02-12 11:32:42 +0530

    Reversing a Linked List

---
+++ 05_Linked-List/reverseLinkList.js
@@ -0,0 +1,12 @@
+function reverseLinkedList(head) {
+  let prev = null;
+  let curr = head;
+  while (curr != null) {
+    let temp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = temp;
+  }
+  head = prev;
+  return head;
+}
