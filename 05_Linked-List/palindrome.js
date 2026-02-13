949bb55a59b6182d851526e1b914b126f12b0c73
Aarsh Singh <starfire9599@gmail.com>
2026-02-13 11:38:15 +0530

    Palindrome, Approach - 2 Optimized

--- 05_Linked-List/palindrome.js
+++ 05_Linked-List/palindrome.js
@@ -17,7 +17,38 @@ function palindrome(head) {
 }
 
 // Approach 2 - Time O(n), Space O(1)
-function palindrome2(head){
-  
-}
+function palindrome2(head) {
+  if (head == null || head.next == null) return true;
 
+  // Find the middle of LL
+  let slow = head;
+  let fast = head;
+
+  while (fast != null && fast.next != null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  // Reverse the second half
+  let prev = null;
+  let curr = slow;
+  while (curr != null) {
+    let temp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = temp;
+  }
+
+  // Find the palindrome
+  let firstList = head;
+  let secondList = prev;
+
+  while (secondList != null) {
+    if (firstList.val !== secondList.val) {
+      return false;
+    }
+    firstList = firstList.next;
+    secondList = secondList.next;
+  }
+  return true;
+}
