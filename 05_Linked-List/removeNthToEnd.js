b1e2c70117768e510a2c585639be8a132538d400
Aarsh Singh <starfire9599@gmail.com>
2026-02-14 21:41:36 +0530

    Approach 2 - In 1 pass

--- 05_Linked-List/removeNthToEnd.js
+++ 05_Linked-List/removeNthToEnd.js
@@ -1,9 +1,9 @@
-// Approach 1 - 2 pass. Time = O(n), Space = O(1)
 function ListNode(val, next) {
   this.val = val === undefined ? 0 : val;
   this.next = next === undefined ? null : next;
 }
 
+// Approach 1 - 2 pass. Time = O(n), Space = O(1)
 function reomveNthFromEnd(head, n) {
   let sentinel = new ListNode();
   sentinel.next = head;
@@ -23,4 +23,21 @@ function reomveNthFromEnd(head, n) {
   return sentinel.next;
 }
 
+// Approach 2 - 1 pass. Time = O(n), Space = O(1)
+function reomveNthFromEnd2(head, n) {
+  let sentinel = new ListNode();
+  sentinel.next = head;
 
+  let slow = sentinel;
+  let fast = sentinel;
+  for (let i = 0; i < n; i++) {
+    fast = fast.next;
+  }
+
+  while (fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+  slow.next = slow.next.next;
+  return sentinel.next;
+}
