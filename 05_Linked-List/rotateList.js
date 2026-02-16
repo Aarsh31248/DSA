29263bbde6e3da29b5b9f1ec9332a43877e7d8df
Aarsh Singh <starfire9599@gmail.com>
2026-02-16 13:09:12 +0530

    Rotate the List k times

---
+++ 05_Linked-List/rotateList.js
@@ -0,0 +1,30 @@
+function rotateListKTimes(head, k) {
+  if (!head || !head.next) return head;
+
+  let curr = head;
+  let length = 1;
+  while (curr.next !== null) {
+    curr = curr.next;
+    length++;
+  }
+
+  k = k % length;
+  if (k === 0) return head;
+
+  let slow = head;
+  let fast = head;
+
+  for (let i = 0; i < k; i++) {
+    fast = fast.next;
+  }
+
+  while (fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next;
+  }
+
+  fast.next = head;
+  let headNode = slow.next;
+  slow.next = null;
+  return headNode;
+}
