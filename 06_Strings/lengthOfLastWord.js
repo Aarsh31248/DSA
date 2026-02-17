93d6dfac931cc676e39056f9894a9c3197c8d64f
Aarsh Singh <starfire9599@gmail.com>
2026-02-17 09:54:55 +0530

    Approach - 2, using 2 while loops

--- 06_Strings/lengthOfLastWord.js
+++ 06_Strings/lengthOfLastWord.js
@@ -1,10 +1,30 @@
+let str = "     fly   me to the moon   ";
+
 // Approach - 1, using In-built function
-function lengthOfLastWord(s) {
-  let words = s.trim().split(" ");
+function lengthOfLastWord(str) {
+  let words = str.trim().split(" ");
   words = words[words.length - 1];
   return words.length;
 }
-let s = "     fly   me to the moon   ";
-// console.log(lengthOfLastWord(s));
+console.log(lengthOfLastWord(str));
 
+// Approach - 2, using 2 while loops
+function lengthOfLastWord2(str) {
+  // Trim the spaces
+  let n = str.length - 1;
+  while (n >= 0) {
+    if (str[n] != " ") break;
+    --n;
+  }
+  // n is at the point where last word starts
+  // Count the character till you reach the space
+  let count = 0;
+  while (n >= 0) {
+    if (str[n] == " ") break;
+    --n;
+    ++count;
+  }
 
+  return count;
+}
+console.log(lengthOfLastWord2(str));
