14053aa7e7481dae781870f7de05f92a40304ffa
Aarsh Singh <starfire9599@gmail.com>
2026-02-17 11:02:44 +0530

    Approach - 2, Tradational way

--- 06_Strings/wordContainChar.js
+++ 06_Strings/wordContainChar.js
@@ -11,4 +11,17 @@ function findWordsContainChar(words, x) {
 }
 console.log(findWordsContainChar(words, x));
 
-
+// Approach - 2, Time = O(n * m), Space = O(n) or O(1) because res is not used for the logic instead used for storing the result that question is demanding
+function findWordsContainChar2(words, x) {
+  let res = [];
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words[i].length; j++) {
+      if (words[i][j] === x) {
+        res.push(i);
+        break;
+      }
+    }
+  }
+  return res;
+}
+console.log(findWordsContainChar2(words, x));
