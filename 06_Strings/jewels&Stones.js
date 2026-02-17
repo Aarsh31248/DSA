2baf2f6424c30d24250bc999cfd25e09ffbfbf9f
Aarsh Singh <starfire9599@gmail.com>
2026-02-17 12:57:57 +0530

    Approach 2, using Set(Optimized)

--- 06_Strings/jewels&Stones.js
+++ 06_Strings/jewels&Stones.js
@@ -8,6 +8,7 @@ function jewelsAndStones(jewels, stones) {
     for (let j = 0; j < jewels.length; j++) {
       if (stones[i] == jewels[j]) {
         res++;
+        break;
       }
     }
   }
@@ -15,4 +16,20 @@ function jewelsAndStones(jewels, stones) {
 }
 console.log(jewelsAndStones(jewels, stones));
 
+// Approach - 2 (optimized using Set), Time = O(n + m), Space = O(m) or O(1) if character set is fixed(max 52)
+function jewelsAndStones2(jewels, stones) {
+  let count = 0;
+  let set = new Set();
 
+  for (let i = 0; i < jewels.length; i++) {
+    set.add(jewels[i]);
+  }
+
+  for (let i = 0; i < stones.length; i++) {
+    if (set.has(stones[i])) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(jewelsAndStones2(jewels, stones));
