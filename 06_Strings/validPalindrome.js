ccf1cadc608b62774f4c6b5d2c54a628b3104f13
Aarsh Singh <starfire9599@gmail.com>
2026-02-18 11:12:33 +0530

    Approach -2, using 2 pointers with Time = O(n), Space = O(1)

--- 06_Strings/validPalindrome.js
+++ 06_Strings/validPalindrome.js
@@ -16,3 +16,25 @@ function validPalindrome(str) {
   return filter === reverse;
 }
 console.log(validPalindrome(str));
+
+// Approach -2, using 2 pointers. Time = O(n), Space = O(1)
+function validPalindrome2(str) {
+  str = str.toLowerCase();
+  let i = 0;
+  let j = str.length - 1;
+
+  while (i < j) {
+    if (!str[i].match(/[a-z0-9]/i)) {
+      i++;
+    } else if (!str[j].match(/[a-z0-9]/i)) {
+      j--;
+    } else if (str[i] === str[j]) {
+      i++;
+      j--;
+    } else {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(validPalindrome2(str));
