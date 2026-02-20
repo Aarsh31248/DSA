cbe9b22e8d25d753d464eb21761214870430b70d
Aarsh Singh <starfire9599@gmail.com>
2026-02-20 12:26:33 +0530

    Valid Parentheses, Approach - 1

---
+++ 07_Stack&Queue/validParentheses.js
@@ -0,0 +1,25 @@
+let s = "(){}[()]{}";
+
+// Approach - 1
+function validParentheses(s) {
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(" || s[i] === "{" || s[i] === "[") {
+      stack.push(s[i]);
+    } else {
+      let top = stack.pop();
+
+      if (
+        !top ||
+        (top === "(" && s[i] !== ")") ||
+        (top === "{" && s[i] !== "}") ||
+        (top === "[" && s[i] !== "]")
+      ) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+}
+console.log(validParentheses(s));
