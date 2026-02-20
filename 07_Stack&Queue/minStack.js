bf3b55be938ba5031e5cf3770f1b94ad02b7a9dd
Aarsh Singh <starfire9599@gmail.com>
2026-02-20 13:17:20 +0530

    Found Minimun value in stack in Time = O(1)

---
+++ 07_Stack&Queue/minStack.js
@@ -0,0 +1,26 @@
+function MinStack() {
+  this.s = [];
+}
+
+MinStack.prototype.push = function (val) {
+  if (this.s.length === 0) {
+    this.s.push([val, val]);
+  } else {
+    let minVal = Math.min(val, this.s[this.s.length - 1][1]);
+    this.s.push([val, minVal]);
+  }
+};
+
+MinStack.prototype.pop = function () {
+  this.s.pop();
+};
+
+MinStack.prototype.top = function () {
+  return this.s[this.s.length - 1][0];
+};
+
+MinStack.prototype.getMin = function () {
+  return this.s[this.s.length - 1][1];
+};
+
+// Time = O(1), Space = O(n)
