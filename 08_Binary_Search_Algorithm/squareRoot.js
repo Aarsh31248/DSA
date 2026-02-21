8c85929dce68afaa1d14399546a664c8a9399116
Aarsh Singh <starfire9599@gmail.com>
2026-02-22 01:06:39 +0530

    Square Root of num, Approach - 2, using Binary Search, Time = O(log n) Space = O(1)

--- 08_Binary_Search_Algorithm/squareRoot.js
+++ 08_Binary_Search_Algorithm/squareRoot.js
@@ -10,4 +10,27 @@ function squareRoot(x) {
 }
 console.log(squareRoot(x));
 
+// Approach - 2, using Binary Search, Time = O(log n) Space = O(1)
+function squareRoot2(x) {
+  if (x < 2) return x;
 
+  let left = 2;
+  let right = Math.floor(x / 2);
+  let ans = 0;
+
+  while (right >= left) {
+    let mid = Math.floor((left + right) / 2);
+    let square = mid * mid;
+
+    if (square === x) return mid;
+
+    if (square < x) {
+      ans = mid;
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return ans;
+}
+console.log(squareRoot2(x));
